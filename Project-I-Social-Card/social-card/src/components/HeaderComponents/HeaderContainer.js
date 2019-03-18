ec0439d3edbb837c4d8613f95660a1be2f672ea9
sriley <sarahriley.dev@gmail.com>
2019-03-18 16:33:07 -0400

    header components built

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,13 +3,14 @@ import "./Header.css";
 
 import ImageThumbnail from "./ImageThumbnail";
 import HeaderContent from "./HeaderContent";
+import HeaderTitle from "./HeaderTitle";
 
 const HeaderContainer = () => {
   return (
     <div className="HeaderContainer">
       <ImageThumbnail />
-      {/* <HeaderContent /> */}
-      {/* <HeaderTitle /> */}
+      <HeaderTitle />
+      <HeaderContent />
     </div>
   );
 };

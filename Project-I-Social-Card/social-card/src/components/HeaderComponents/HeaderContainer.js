e2c1d2c5027a09dec1b7d9684661c40780409f7e
sriley <sarahriley.dev@gmail.com>
2019-03-18 16:13:06 -0400

    thumbnail rendering

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderContent from "./HeaderContent";
 
 const HeaderContainer = () => {
   return (
-    <div className="HeaderContianer">
+    <div className="HeaderContainer">
+      <ImageThumbnail />
+      {/* <HeaderContent /> */}
+      {/* <HeaderTitle /> */}
     </div>
   );
 };
 
 export default HeaderContainer;
-

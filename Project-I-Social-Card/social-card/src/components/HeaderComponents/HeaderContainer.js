302963a2a796497e6ce2bc63e7690964399725b0
sriley <sarahriley.dev@gmail.com>
2019-03-18 15:25:22 -0400

    building header container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import './Header.css';
 
+const HeaderContainer = () => {
+  return (
+    <div className="HeaderContianer">
+    </div>
+  );
+};
+
+export default HeaderContainer;
 

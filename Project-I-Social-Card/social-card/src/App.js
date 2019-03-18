9977face5055ba3dd1d3fa1940e2d6addddb1998
sriley <sarahriley.dev@gmail.com>
2019-03-18 16:48:19 -0400

    card container rendering

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from '../src/components/HeaderComponents/HeaderContainer';
+import CardContainer from '../src/components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className="App">
     <HeaderContainer />
+    <CardContainer />
   </div>
   );
 };
